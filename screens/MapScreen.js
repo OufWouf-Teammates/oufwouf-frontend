b54e7166c42b76809a20cae3039e94b8431a5114
Clément N <nouet.clem@gmail.com>
2024-12-10 13:52:15 +0100

    maps updated

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,46 +1,31 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
 
 export default function MapScreen() {
-  const [currentPosition, setCurrentPosition] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-    (async () => {
-      const { status } = await Location.requestForegroundPermissionsAsync();
-   
-      if (status === 'granted') {
-        Location.watchPositionAsync({ distanceInterval: 10 },
-          (location) => {
-            setCurrentPosition(location.coords);
-            console.log(location);
-          });
-      }
-    })();
-   }, []);
-
+  // Coordonnées fixes pour le Marker
+  const markerPosition = {
+    latitude: 48.8566,  // Latitude pour Paris
+    longitude: 2.3522,  // Longitude pour Paris
+  };
 
   return (
-    <MapView style={styles.map}>
-      {currentPosition && (
-        <Marker
-        coordinate={{
-          latitude: currentPosition.latitude,
-          longitude: currentPosition.longitude,
-          }}
-          title="My Position"
-          />
-          )}
+    <MapView style={styles.map} initialRegion={{
+      latitude: 48.8566,  // Latitude de départ pour le centre de la carte
+      longitude: 2.3522,  // Longitude de départ pour le centre de la carte
+      latitudeDelta: 0.0922, // Zoom de la carte
+      longitudeDelta: 0.0421, // Zoom de la carte
+    }}>
+      <Marker
+        coordinate={markerPosition}
+        title="Marker Example"
+        description="C'est un marker avec une latitude et une longitude précises."
+      />
     </MapView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
     flex: 1,
   },
