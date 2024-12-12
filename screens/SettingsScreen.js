318812754cba49d1a7c93b26c14f2e07a3b0f9fc
Diego <gomeztorres.diegoandres@gmail.com>
2024-12-12 14:08:09 +0100

    Mise en place style settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,6 +2,12 @@ import { Button, StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackg
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
+import {
+    useFonts,
+    Lexend_400Regular,
+    Lexend_700Bold,
+  } from "@expo-google-fonts/lexend"
+  import AppLoading from "expo-app-loading"
 
 export default function SettingsScreen({ navigation }) {
     const token = useSelector((state) => state.user.value.token)
@@ -20,8 +26,18 @@ export default function SettingsScreen({ navigation }) {
           const data = await response.json()
     
           setDog(data.dog[0])
+          console.log(data.dog[0]);
+          
         })()
-      }, [])
+    }, [])
+    //Nécessaire pour la configuration des fonts
+    const [fontsLoaded] = useFonts({
+        Lexend_400Regular,
+        Lexend_700Bold,
+    })
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
  return (
     <ImageBackground
       source={require("../assets/BG_App.png")}
@@ -36,10 +52,38 @@ export default function SettingsScreen({ navigation }) {
             </TouchableOpacity>
             <View style={styles.in}>
                 <Image
-                    ssource={dog?.uri ? { uri: dog.uri } : require("../assets/chien.png")}
-                    style={styles.dogPic}
+                    source={dog?.uri ? { uri: dog.uri } : require("../assets/chien.png")}
+                    style={[styles.dogPic, {width: 150, height: 150}]}
                 />
-                 <Text>{dog.name}</Text> 
+                 <Text style={styles.name}>{dog?.name || 'Tuu'}</Text> 
+                 <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.8}
+                >
+                    <FontAwesome name="paw" size={25} color="#0639DB" /> 
+                    <Text style={styles.textButton}>Information personal</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.8}
+                >
+                    <FontAwesome name="bell" size={25} color="#0639DB" /> 
+                    <Text style={styles.textButton}>Notifications</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.8}
+                >
+                    <FontAwesome name="calendar" size={25} color="#0639DB" /> 
+                    <Text style={styles.textButton}>Agenda</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonParam}
+                    activeOpacity={0.8}
+                >
+                    <FontAwesome name="gear" size={25} color="#FFF" /> 
+                    <Text style={styles.textButtonParam}>Paramètres</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
         
@@ -54,17 +98,53 @@ const styles = StyleSheet.create({
     },
     innerContainer:{
         flex: 1,
-        width: "80%",
+        width: "90%",
+        height: '100%',
     },
     in: {
         width: '100%',
         alignItems: 'center',
     },
     dogPic: {
-        width: "40%",
         aspectRatio: 1,
         borderRadius: 500,
-        borderWidth: 2,
         marginVertical: 20,
     },
+    name: {
+        fontFamily: 'Lexend_700Bold',
+        fontSize: 24,
+    },
+    button: {
+        backgroundColor: "#FFF",
+        width: "90%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        marginVertical: 10,
+        padding: 15,
+        borderRadius: 5,
+      },
+      textButton: {
+        color: "#0639DB",
+        fontSize: 16,
+        width: "70%",
+        fontFamily: "Lexend_400Regular",
+      },
+      buttonParam: {
+        backgroundColor: "#0639DB",
+        width: "90%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        marginVertical: 100,
+        padding: 15,
+        borderRadius: 5,
+        
+      },
+      textButtonParam: {
+        color: "#FFF",
+        fontSize: 16,
+        width: "70%",
+        fontFamily: "Lexend_400Regular",
+      },
   });
