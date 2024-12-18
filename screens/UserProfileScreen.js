a286de931e6b273891b2c727f533913aa2216cb2
Raphael <raphael.chagny@gmail.com>
2024-12-18 15:50:44 +0100

    ajout du userProgile

--- screens/UserProfileScreen.js
+++ screens/UserProfileScreen.js
@@ -33,16 +33,18 @@ import NestedModalCalendar from "../components/NestedModalCalendar"
 
 const UserProfileScreen = ({ navigation, route }) => {
   const token = useSelector((state) => state.user.value.token)
-  const [dogData, setDogData] = useState([])
-  const apiGetUser = `${process.env.EXPO_PUBLIC_BACKEND_URL}`
+  const [userData, setUserData] = useState(null)
+  const apiGetUser = `${process.env.EXPO_PUBLIC_BACKEND_URL}users/dogname`
 
   useEffect(() => {
     ;(async () => {
-      const getDog = await fetch(apiGetUser)
+      const getDog = await fetch(`${apiGetUser}?name=${route.name}`)
 
       const response = await getDog.json()
 
-      setDogData(response)
+      console.log(response)
+
+      setUserData(response)
     })()
   }, [])
 
@@ -63,17 +65,21 @@ const UserProfileScreen = ({ navigation, route }) => {
       >
         <FontAwesome name="arrow-left" size={30} color="#0639DB" />
       </TouchableOpacity>
-      <Text>{route.dogName}</Text>
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1,
-          width: "100%",
-          paddingBottom: 20,
-        }}
-      >
-        <View style={styles.innerContainer}>
-          <Image source={{ uri: dogData?.uri }} style={styles.dogPic} />
-        </View>
+
+      <ScrollView contentContainerStyle={styles.innerContainer}>
+        <Image
+          source={{
+            uri: userData?.profilePicture || "https://via.placeholder.com/150",
+          }}
+          style={styles.profilePicture}
+        />
+
+        <Text style={styles.name}>
+          {userData?.name || "Nom non disponible"}
+        </Text>
+        <Text style={styles.email}>
+          {userData?.email || "Email non disponible"}
+        </Text>
       </ScrollView>
     </ImageBackground>
   )
@@ -86,58 +92,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   innerContainer: {
-    flex: 1,
-    width: "100%",
     alignItems: "center",
-    justifyContent: "flex-start",
-    gap: 40,
     marginTop: 100,
-  },
-  dogPic: {
-    width: "50%",
-    marginBottom: 35,
-    aspectRatio: 1,
-    borderRadius: 500,
-  },
-  infos: {
-    width: "90%",
-    gap: 20,
-  },
-  demiBox: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  updatePhoto: {
-    backgroundColor: "white",
-    width: 30,
-    height: 30,
-    borderRadius: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: 170,
-    right: 0,
-  },
-  infoBox: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    padding: 15,
-    backgroundColor: "white",
-    borderRadius: 5,
-  },
-  dogInfo: {
-    width: "90%",
-    fontFamily: "Lexend_400Regular",
-  },
-  infoDemiBox: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "48%",
-    paddingVertical: 15,
-    paddingHorizontal: 25,
-    backgroundColor: "white",
-    borderRadius: 5,
+    paddingBottom: 50,
   },
   iconBack: {
     position: "absolute",
@@ -145,101 +102,35 @@ const styles = StyleSheet.create({
     left: 30,
     zIndex: 50,
   },
-  vaccins: {
-    gap: 15,
+  profilePicture: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+    borderWidth: 2,
+    borderColor: "#0639DB",
   },
   name: {
+    fontSize: 24,
     fontWeight: "bold",
-    fontSize: 30,
-    marginTop: -40,
     fontFamily: "Lexend_700Bold",
+    color: "#0639DB",
+    marginBottom: 10,
   },
-  icons: {
-    marginRight: 15,
-    marginLeft: 15,
+  email: {
+    fontSize: 18,
+    fontFamily: "Lexend_400Regular",
+    color: "#4D4D4D",
   },
-  iconsDemi: {
-    marginRight: 5,
-    marginLeft: 5,
-  },
-  infoModal: {
+  loadingContainer: {
     flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.2)",
-  },
-  modalViewInfo: {
-    width: "90%",
-    height: "90%",
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 5,
-    padding: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  textStyleInfo: {
-    color: "#0639DB",
-    width: "100%",
-    textAlign: "center",
-    fontFamily: "Lexend_700Bold",
-    fontSize: 20,
-    marginVertical: 10,
-  },
-  text: {
-    color: "#0639DB",
-    width: "90%",
-    paddingLeft: 15,
-    fontFamily: "Lexend_400Regular",
-    fontSize: 16,
-    marginVertical: 10,
-  },
-  input: {
-    height: 60,
-    borderColor: "#4D4D4D",
-    backgroundColor: "#BFBFBF",
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingLeft: 10,
-    borderRadius: 5,
-    color: "black",
-    fontSize: 16,
-    width: "90%",
-    borderBottomWidth: 1,
-    fontFamily: "Lexend_400Regular",
-    alignSelf: "center",
-  },
-  inputFocused: {
-    backgroundColor: "#F3E882",
-    borderColor: "#FBBC05",
-  },
-  buttonModalSettings: {
-    backgroundColor: "#0639DB",
-    width: "100%",
-    flexDirection: "row",
     justifyContent: "center",
-    alignContent: "center",
-    marginVertical: 10,
-    padding: 15,
-    borderRadius: 5,
-    alignSelf: "center",
+    alignItems: "center",
   },
-  textStyleSettings: {
-    color: "#FFF",
-    width: "100%",
-    textAlign: "center",
+  loadingText: {
+    fontSize: 18,
     fontFamily: "Lexend_400Regular",
-    fontSize: 16,
-    marginRight: 5,
-  },
-  close: {
-    marginBottom: 10,
+    color: "#4D4D4D",
   },
 })
 
