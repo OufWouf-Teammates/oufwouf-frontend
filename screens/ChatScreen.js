d2c8bb172df7dc566d34ac99833c00eb436a43cd
Your name <Your email>
2024-12-20 10:14:10 +0100

    ca chat

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,151 +1,151 @@
-import React, { useEffect, useState } from "react"
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Image,
-  TouchableOpacity,
-  Text,
-  ImageBackground,
-  ActivityIndicator,
-  ScrollView,
-  Linking,
-  TextInput,
-} from "react-native"
-import { FlatList } from "react-native"
-import FontAwesome from "react-native-vector-icons/FontAwesome"
-import {
-  useFonts,
-  Lexend_400Regular,
-  Lexend_700Bold,
-} from "@expo-google-fonts/lexend"
-import AppLoading from "expo-app-loading"
-import { useSelector } from "react-redux"
+// import React, { useEffect, useState } from "react"
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   View,
+//   Image,
+//   TouchableOpacity,
+//   Text,
+//   ImageBackground,
+//   ActivityIndicator,
+//   ScrollView,
+//   Linking,
+//   TextInput,
+// } from "react-native"
+// import { FlatList } from "react-native"
+// import FontAwesome from "react-native-vector-icons/FontAwesome"
+// import {
+//   useFonts,
+//   Lexend_400Regular,
+//   Lexend_700Bold,
+// } from "@expo-google-fonts/lexend"
+// import AppLoading from "expo-app-loading"
+// import { useSelector } from "react-redux"
 
-const DiscussionsScreen = ({ navigation, route }) => {
-  const [search, setSearch] = useState("")
-  const [dogName, setDogName] = useState(route.dogName)
-  const [messages, setMessages] = useState([])
-  const apiRoom = `${process.env.EXPO_PUBLIC_BACKEND_URL}rooms`
-  const apiMessage = `${process.env.EXPO_PUBLIC_BACKEND_URL}messages`
-  const userToken = useSelector((state) => state.user.value?.token)
-  console.log(userToken)
+// const DiscussionsScreen = ({ navigation, route }) => {
+//   const [search, setSearch] = useState("")
+//   const [dogName, setDogName] = useState(route.dogName)
+//   const [messages, setMessages] = useState([])
+//   const apiRoom = `${process.env.EXPO_PUBLIC_BACKEND_URL}rooms`
+//   const apiMessage = `${process.env.EXPO_PUBLIC_BACKEND_URL}messages`
+//   const userToken = useSelector((state) => state.user.value?.token)
+//   console.log(userToken)
 
-  useEffect(() => {
-    ;(async () => {
-      const getMessages = await fetch(`${apiRoom}?name=${dogName}`)
+//   useEffect(() => {
+//     ;(async () => {
+//       const getMessages = await fetch(`${apiRoom}?name=${dogName}`)
 
-      const response = await getMessages.json()
+//       const response = await getMessages.json()
 
-      setMessages(response.messages)
-    })()
-  }, [])
+//       setMessages(response.messages)
+//     })()
+//   }, [])
 
-  const sendMessage = async () => {
-    const send = await fetch(`${apiMessage}?name=${dogName}`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${userToken}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        message: search,
-      }),
-    })
+//   const sendMessage = async () => {
+//     const send = await fetch(`${apiMessage}?name=${dogName}`, {
+//       method: "POST",
+//       headers: {
+//         Authorization: `Bearer ${userToken}`,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         message: search,
+//       }),
+//     })
 
-    const response = await send.json()
-  }
+//     const response = await send.json()
+//   }
 
-  return (
-    <ImageBackground
-      source={require("../assets/BG_App.png")}
-      style={styles.container}
-    >
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        style={styles.iconBack}
-      >
-        <FontAwesome name="arrow-left" size={30} color="#0639DB" />
-      </TouchableOpacity>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}></View>
-        <ScrollView style={styles.messages}>
-          <View style={styles.message}>
-            <Text>
-              voil voila voilavoilavoila voilavoilavoilavoilavoilavoilavoila
-              voila voila voila voila voilavoilavoila voilavoila voila
-              voilavoilaa
-            </Text>
-          </View>
-          <View style={styles.message}>
-            <Text>voila</Text>
-          </View>
-          <View style={styles.message}>
-            <Text>voila</Text>
-          </View>
-          <View style={styles.message}>
-            <Text>voila</Text>
-          </View>
-          {messages &&
-            messages.map((e, i) => (
-              <View key={i} style={styles.message}>
-                <Text>{e.content}</Text>
-              </View>
-            ))}
-        </ScrollView>
-        <View style={styles.sender}>
-          <TextInput
-            style={styles.inputMessage}
-            placeholder="Envoyer un message..."
-            value={search}
-            onChangeText={(e) => setSearch(e)}
-          ></TextInput>
-          <TouchableOpacity
-            style={styles.iconSearch}
-            onPress={() => sendMessage()}
-          >
-            <FontAwesome name="arrow-right" size={25} color="#0639DB" />
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
-    </ImageBackground>
-  )
-}
+//   return (
+//     <ImageBackground
+//       source={require("../assets/BG_App.png")}
+//       style={styles.container}
+//     >
+//       <TouchableOpacity
+//         onPress={() => navigation.goBack()}
+//         style={styles.iconBack}
+//       >
+//         <FontAwesome name="arrow-left" size={30} color="#0639DB" />
+//       </TouchableOpacity>
+//       <SafeAreaView style={styles.container}>
+//         <View style={styles.header}></View>
+//         <ScrollView style={styles.messages}>
+//           <View style={styles.message}>
+//             <Text>
+//               voil voila voilavoilavoila voilavoilavoilavoilavoilavoilavoila
+//               voila voila voila voila voilavoilavoila voilavoila voila
+//               voilavoilaa
+//             </Text>
+//           </View>
+//           <View style={styles.message}>
+//             <Text>voila</Text>
+//           </View>
+//           <View style={styles.message}>
+//             <Text>voila</Text>
+//           </View>
+//           <View style={styles.message}>
+//             <Text>voila</Text>
+//           </View>
+//           {messages &&
+//             messages.map((e, i) => (
+//               <View key={i} style={styles.message}>
+//                 <Text>{e.content}</Text>
+//               </View>
+//             ))}
+//         </ScrollView>
+//         <View style={styles.sender}>
+//           <TextInput
+//             style={styles.inputMessage}
+//             placeholder="Envoyer un message..."
+//             value={search}
+//             onChangeText={(e) => setSearch(e)}
+//           ></TextInput>
+//           <TouchableOpacity
+//             style={styles.iconSearch}
+//             onPress={() => sendMessage()}
+//           >
+//             <FontAwesome name="arrow-right" size={25} color="#0639DB" />
+//           </TouchableOpacity>
+//         </View>
+//       </SafeAreaView>
+//     </ImageBackground>
+//   )
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-  },
-  header: {
-    width: "100%",
-    height: 150,
-  },
-  messages: {},
-  message: {
-    backgroundColor: "white",
-    padding: 10,
-    margin: 20,
-    borderRadius: 10,
-    textAlign: "justify",
-  },
-  iconBack: {
-    position: "absolute",
-    top: 60,
-    left: 30,
-    zIndex: 50,
-  },
-  sender: {
-    flexDirection: "row",
-    backgroundColor: "white",
-    width: "90%",
-    borderRadius: 10,
-    position: "absolute",
-    bottom: 20,
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10,
-  },
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//   },
+//   header: {
+//     width: "100%",
+//     height: 150,
+//   },
+//   messages: {},
+//   message: {
+//     backgroundColor: "white",
+//     padding: 10,
+//     margin: 20,
+//     borderRadius: 10,
+//     textAlign: "justify",
+//   },
+//   iconBack: {
+//     position: "absolute",
+//     top: 60,
+//     left: 30,
+//     zIndex: 50,
+//   },
+//   sender: {
+//     flexDirection: "row",
+//     backgroundColor: "white",
+//     width: "90%",
+//     borderRadius: 10,
+//     position: "absolute",
+//     bottom: 20,
+//     justifyContent: "space-between",
+//     alignItems: "center",
+//     padding: 10,
+//   },
+// })
 
-export default DiscussionsScreen
+// export default DiscussionsScreen
--- screens/ChatScreenCopy.js
+++ screens/ChatScreenCopy.js
@@ -16,7 +16,6 @@ import { Audio } from "expo-av";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import Pusher from "pusher-js/react-native";
 
-const pusher = new Pusher("PUSHER_KEY", { cluster: "PUSHER_CLUSTER" });
 const BACKEND_ADDRESS = process.env.EXPO_PUBLIC_BACKEND_URL;
 
 export default function ChatScreen({ navigation, route: { params } }) {
@@ -28,23 +27,27 @@ export default function ChatScreen({ navigation, route: { params } }) {
   const [recordingUri, setRecordingUri] = useState(null);
   const [sound, setSound] = useState(null);
 
-  const token = useSelector((state) => state.user.value?.token)
+  const token = useSelector((state) => state.user.value?.token);
 
   const channelName = `room-${roomName.replace(/\s+/g, "-").toLowerCase()}`;
+  const newUser = `${username.replace(/\s+/g, "-").toLowerCase()}`;
 
   const getUserName = async () => {
     try {
       const response = await fetch(`${BACKEND_ADDRESS}dogName/${token}`);
       const data = await response.json();
-      setUsername(data?.dogName[0])
-  
+      setUsername(data?.dogName);
+
       if (data.dogName) {
-        const joinRes = await fetch(`${BACKEND_ADDRESS}users/${data?.dogName[0]}`, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ roomName }),
-        });
-  
+        const joinRes = await fetch(
+          `${BACKEND_ADDRESS}users/${data?.dogName}`,
+          {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ roomName }),
+          }
+        );
+
         if (!joinRes.ok) {
           console.error("Erreur lors de la jonction de la salle");
         }
@@ -53,22 +56,14 @@ export default function ChatScreen({ navigation, route: { params } }) {
       console.error("Erreur dans getUserName :", error);
     }
   };
-  
 
   useEffect(() => {
-
-    getUserName()
-
-    // Subscribe to Pusher channel
-    const subscription = pusher.subscribe(channelName);
-    subscription.bind("pusher:subscription_succeeded", () => {
-      subscription.bind("message", handleReceiveMessage);
-    });
+    getUserName();
 
     // Cleanup on unmount
     return async () => {
       try {
-        await fetch(`${BACKEND_ADDRESS}users/${username}`, {
+        await fetch(`${BACKEND_ADDRESS}users/${newUser}`, {
           method: "DELETE",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ roomName }),
@@ -78,8 +73,23 @@ export default function ChatScreen({ navigation, route: { params } }) {
       }
       subscription.unsubscribe();
     };
-    
   }, [username]);
+
+  useEffect(() => {
+    const pusher = new Pusher("33b9c1445a837cef17f5", { cluster: "eu" });
+
+    // Subscribe to Pusher channel
+    const subscription = pusher.subscribe(channelName);
+    subscription.bind("pusher:subscription_succeeded", () => {
+      subscription.bind("message", (data) => {
+        handleReceiveMessage(data);
+      });
+    });
+
+    return () => {
+      pusher.unsubscribe(channelName);
+    };
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -98,6 +108,7 @@ export default function ChatScreen({ navigation, route: { params } }) {
   }, [sound]);
 
   const handleReceiveMessage = (data) => {
+    console.log("data received =>", data);
     setMessages((messages) => [...messages, data]);
   };
 
@@ -267,3 +278,155 @@ export default function ChatScreen({ navigation, route: { params } }) {
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#000",
+  },
+  inset: {
+    flex: 1,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    backgroundColor: "#ffffff",
+    width: "100%",
+    paddingTop: 20,
+    position: "relative",
+    borderTopColor: "#ffe099",
+    borderLeftColor: "#ffe099",
+    borderRightColor: "#ffe099",
+    borderTopWidth: 4,
+    borderRightWidth: 0.1,
+    borderLeftWidth: 0.1,
+  },
+  banner: {
+    width: "100%",
+    height: "15%",
+    paddingTop: 20,
+    paddingLeft: 20,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  greetingText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 18,
+    marginLeft: 15,
+  },
+  message: {
+    paddingTop: 12,
+    paddingBottom: 12,
+    paddingRight: 20,
+    paddingLeft: 20,
+    borderRadius: 24,
+    alignItems: "flex-end",
+    justifyContent: "center",
+    maxWidth: "65%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6.41,
+    elevation: 1.2,
+  },
+  messageWrapper: {
+    alignItems: "flex-end",
+    marginBottom: 20,
+  },
+  messageRecieved: {
+    alignSelf: "flex-end",
+    alignItems: "flex-end",
+  },
+  messageSent: {
+    alignSelf: "flex-start",
+    alignItems: "flex-start",
+  },
+  messageSentBg: {
+    backgroundColor: "#ffad99",
+  },
+  messageRecievedBg: {
+    backgroundColor: "#d6fff9",
+  },
+  messageText: {
+    color: "#506568",
+    fontWeight: "400",
+  },
+  timeText: {
+    color: "#506568",
+    opacity: 0.5,
+    fontSize: 10,
+    marginTop: 2,
+  },
+  inputContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    justifySelf: "flex-end",
+    alignContent: "flex-start",
+    marginBottom: 30,
+    marginTop: "auto",
+    background: "transparent",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  input: {
+    backgroundColor: "#f0f0f0",
+    width: "60%",
+    padding: 14,
+    borderRadius: 30,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6.41,
+    elevation: 1.2,
+  },
+  recordButton: {
+    borderRadius: 50,
+    padding: 16,
+    backgroundColor: "#ff5c5c",
+    marginLeft: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6.41,
+    elevation: 1.2,
+  },
+  sendButton: {
+    borderRadius: 50,
+    padding: 16,
+    backgroundColor: "#ffe099",
+    marginLeft: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6.41,
+    elevation: 1.2,
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontWeight: "800",
+    textTransform: "uppercase",
+  },
+  scroller: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
